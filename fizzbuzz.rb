c372a5a4836f0397bd07c32b53f8f471036aea9f
Matthew Lee <yleematthew@gmail.com>
2019-02-05 05:31:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,6 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
     if int % 3 == 0 # if the number int is divisible by 3
-      "Fizz" # Go fizz
-    end
-    if int % 3 == 0 # if the number int is divisible by 3
      return  "Fizz" # Go fizz
    end
     if int % 5 == 0
